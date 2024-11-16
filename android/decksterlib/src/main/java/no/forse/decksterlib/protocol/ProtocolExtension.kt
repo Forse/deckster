c8040b46699df6e7e2c906b261cd45eb7d69b94e
Frode Nilsen <frode.nilsen@forse.no>
2024-11-16 13:28:09 +0100

    Fix issue in CrazyEights putEight

--- android/decksterlib/src/main/java/no/forse/decksterlib/protocol/ProtocolExtension.kt
+++ android/decksterlib/src/main/java/no/forse/decksterlib/protocol/ProtocolExtension.kt
@@ -1,5 +1,9 @@
 package no.forse.decksterlib.protocol
 
 import no.forse.decksterlib.model.protocol.DecksterMessage
+import kotlin.reflect.KClass
 
 fun DecksterMessage.getType(): String = this.javaClass.name.replace("no.forse.decksterlib.model.", "")
+
+fun <T> dtoType(clazz: KClass<T>):  String where T: DecksterMessage =
+    clazz.qualifiedName.toString().replace("no.forse.decksterlib.model.", "")
