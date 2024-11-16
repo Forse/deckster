ebc11c9c176e32921928f66aa276acf6699e8218
David Kristensen <david.kristensen@bym.oslo.kommune.no>
2024-11-16 13:38:00 +0100

    legger til texas hold em

---
+++ src/Deckster.Core/Games/TexasHoldEm/TexasHoldEmRequests.cs
@@ -0,0 +1,14 @@
+﻿using Deckster.Core.Protocol;
+
+namespace Deckster.Core.Games.TexasHoldEm;
+
+public class BetRequest : DecksterRequest
+{
+    public int Bet { get; set; }
+}
+
+public class FoldRequest : DecksterRequest;
+
+public class CheckRequest : DecksterRequest;
+
+public class CallRequest : DecksterRequest;
